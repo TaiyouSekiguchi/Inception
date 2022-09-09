96e664f7d3da2611f30eaf137480dc48dab6523d
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-09 17:04:12 +0900

    roleを修正。

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,7 +1,7 @@
 if [ ! -e "/var/www/html/wp-config.php" ]; then
     wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
     wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${ADMIN_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
-    wp user create ${DB_USER} ${USER_EMAIL} --allow-root --role=subscriber --user_pass=${DB_PASSWORD}
+    wp user create ${DB_USER} ${USER_EMAIL} --allow-root --role=editor --user_pass=${DB_PASSWORD}
 fi
 
 exec /usr/sbin/php-fpm7.3 --nodaemonize
