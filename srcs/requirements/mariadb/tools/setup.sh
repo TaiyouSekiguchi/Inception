15fa94384e2596ed2af05e81d27c7d24ff573183
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-09 10:48:13 +0900

    typo を修正。

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-set -i "s/#port/port /g" /etc/mysql/mariadb.conf.d/50-server.cnf
-set -i "s/bind-address/# bind-address/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/#port/port /g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/bind-address/# bind-address/g" /etc/mysql/mariadb.conf.d/50-server.cnf
