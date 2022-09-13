32bcc1e3b28171ecfc4b861bb65db9cfcfb9312e
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-13 18:03:53 +0900

    wordpressをwp-cliでインストール

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
-if [ ! -e "/var/www/html/wp-config.php" ]; then
-    wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
-    wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${ADMIN_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
-    wp user create ${DB_USER} ${USER_EMAIL} --allow-root --role=editor --user_pass=${DB_PASSWORD}
+wp core is-installed --allow-root 2> /dev/null
+if [ $? -ne 0 ]; then
+    wp core download --allow-root
+
+    wp config create \
+        --dbhost=${DB_HOST} \
+        --dbname=${DB_NAME} \
+        --dbuser=${DB_USER} \
+        --dbpass=${DB_PASSWORD} \
+        --allow-root \
+        --skip-check
+
+    wp core install \
+        --url=${DOMAIN_NAME} \
+        --title=${TITLE} \
+        --admin_user=${ADMIN_USER} \
+        --admin_password=${DB_ROOT_PASSWORD} \
+        --admin_email=${ADMIN_EMAIL} \
+        --skip-email \
+        --allow-root
+
+    wp user create \
+        ${DB_USER} \
+        ${USER_EMAIL} \
+        --user_pass=${DB_PASSWORD} \
+        --allow-root \
+        --role=editor
+
+    chown -R www-data:www-data *
+    chmod -R 775 /var/www/*
 fi
 
 exec /usr/sbin/php-fpm7.3 --nodaemonize
