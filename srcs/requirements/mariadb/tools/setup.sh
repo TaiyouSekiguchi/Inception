2fec6cde473aeee4b6e889302803e967c9463ebc
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-13 11:07:31 +0900

    試行錯誤中。

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sed -i 's/#port/port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i 's/bind-address/# bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
