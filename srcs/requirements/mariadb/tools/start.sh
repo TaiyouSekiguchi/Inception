35a5e11f2bc03894e77cf7caf2d553eccd1eabd2
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-13 14:08:09 +0900

    修正。

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -3,18 +3,18 @@
 chown -R mysql:mysql /var/lib/mysql
 if ! [ -d "/var/lib/mysql/wordpress" ]; then
     service mysql start
-    sleep 3
-    #for i in {30..1}; do
-        #if echo 'SELECT 1' | mysql -u root; then
-            #break
-        #fi
-        #echo 'MySQL init process in progress...'
-        #sleep 1
-    #done
-    #if [ "$i" = 0 ]; then
-        #echo >&2 'MySQL init process failed.'
-        #exit 1
-    #fi
+    #sleep 3
+    for i in {30..1}; do
+        if ! echo 'SELECT 1' | mysql -u root; then
+            break
+        fi
+        echo 'MySQL init process in progress...'
+        sleep 1
+    done
+    if [ "$i" = 0 ]; then
+        echo >&2 'MySQL init process failed.'
+        exit 1
+    fi
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
     mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
